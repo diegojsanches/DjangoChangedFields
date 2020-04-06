e7791c6156dfc5b67e07eee74447ce8f09ef6396
diego <diegojsanches@hotmail.com>
2020-04-06 11:03:30 -0300

    First Commit Mixin

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+setup(
+    name='django_changed_fields',
+    version='0.1.0',
+    description='Testing installation of Package',
+    url='#',
+    author='Diego Sanches',
+    author_email='diegojsanches@hotmail.com',
+    license='MIT',
+    packages=['django_changed_fields'],
+    zip_safe=False
+)
