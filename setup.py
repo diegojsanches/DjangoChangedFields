e5b1fbae033026dd6e1db0adc53808c24b570275
diego <diegojsanches@hotmail.com>
2021-10-19 00:31:39 -0300

    update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
     name='django_changed_fields',
-    version='0.2.4',
+    version='0.3.0',
     description='Testing installation of Package',
     url='https://github.com/diegojsanches/DjangoChangedFields',
     author='Diego Sanches',
